133d7e0e11717df9fbaf38973002cfedc3b73568
Pato Programador <spartan117947@hotmail.com>
2022-11-15 11:05:51 -0500

    Solucion del primer homework

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,25 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  let numS = num.split("").reverse();
+  let numDecimal = 0;
+  for (let i = 0; i < numS.length; i++) {
+    let sum = (numS[i] * 2 ** i);
+    numDecimal += sum;
+  }
+  return numDecimal;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let arrayBinario = []
+  while (num !== 0) {
+    let residuo = num % 2;
+    num = Math.floor(num / 2);
+    arrayBinario.unshift(residuo);
+  }
+  arrayBinario = arrayBinario.join("");
+  return arrayBinario;
 }
 
 
