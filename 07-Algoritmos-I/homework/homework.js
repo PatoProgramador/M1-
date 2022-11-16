4070fe1d71c2973b9df7be05ab28616e616b9df3
Pato Programador <spartan117947@hotmail.com>
2022-11-16 18:44:38 -0500

    Solucion de factorear, bubbleSort, InsertionSort y selectionSort

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -1,12 +1,19 @@
 'use strict'
 // No cambies los nombres de las funciones.
-
 function factorear(num) {
   // Factorear el número recibido como parámetro y devolver en un array
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
+  let primos = [1];
+  let i = 2
+  while (num !== 1) {
+    if (num % i === 0) {
+      primos.push(i)
+      num = num / i
+    } else i++;
+  }
+  return primos;
 }
 
 function bubbleSort(array) {
@@ -14,7 +21,25 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  // // Si intentamos con un solo for solo se van a voltear de dos en dos pero no se va a organizar
+  // // el array en su totalidad
+  // // ej: var miarr = [4, 2, 6, 7, 1, 3];
+  // //     console.log(bubbleSort(miarr)); => [ 2, 4, 6, 1, 3, 7 ]
+  // // for (let i = 0; i < array.length; i++) {
+  // //   if(array[i] > array[i + 1]) {
+  // //     [array[i],array[i+1]] = [array[i+1],array[i]]
+  // //   }
+  // // }
+  // // return array;
+  // // }
+  for (let i = 2; i < array.length; i++) {
+    for (let j = 0; j <= (array.length - i); j++) {
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]]
+      }
+    }
+  }
+  return array;
 }
 
 
@@ -23,7 +48,16 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for(let i = 1; i < array.length; i ++){
+    let aux = array[i];
+    let j = i - 1;
+    while (j >= 0 && array[j] > aux) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = aux;
+  }
+  return array;
 }
 
 
@@ -32,7 +66,34 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  // for(let i = 0; i < array.length - 1; i++) {
+  //   let minIndex = i;
+  //   // creamos otro for para que por cada iteracion del primero le segundo haga todo 
+  //   // el recorrido buscando el menor
+  //   for(let j = i + 1; j < array.length; j++) {
+  //     // la busqueda del minimo
+  //     if (array[j] < array[minIndex]) minIndex = j;
+  //   }
+  //   if(i !== minIndex) {
+  //     let aux = array[i];
+  //     array[i] = array[minIndex];
+  //     array[minIndex] = aux;
+  //   }
+  //   return array;
+  // }
+  for (let i = 0; i < array.length; i++) {
+    let menor = i;
+    for (let j = i+1; j < array.length; j++) {
+      if (array[j] < array[menor]){
+        menor = j;
+      }
+    }
+    if (menor !== i){
+      [array[i], array[menor]] = [array[menor], array[i]];
+    }
+    
+  }
+  return array;
 }
 
 
