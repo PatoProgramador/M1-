0932a2d715a53a540fbe25afbbadab4368688cb4
Pato Programador <spartan117947@hotmail.com>
2022-11-17 18:57:02 -0500

    Solucion del hw, funciones quicksort y mergesort

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -7,6 +7,23 @@ function quickSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  if (array.length <= 1) return array;
+
+  let pivot = array[0];
+  let left = [];
+  let right = [];
+  let middle = [];
+  
+  for (let i = 0; i < array.length; i++) {
+    // for(const num of array)
+    // if (num < pivot) left.push(num);
+    // if (num > pivot) right.push(num);
+    // if (num === pivot) middle.push(num);
+    array[i] < pivot && left.push(array[i]);
+    array[i] > pivot && right.push(array[i]);
+    array[i] === pivot && middle.push(array[i]);
+  }
+  return quickSort(left).concat(middle).concat(quickSort(right));
 }
 
 function mergeSort(array) {
@@ -14,7 +31,31 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if (array.length <= 1) return array;
+  // división del array
+  const middleIndex = Math.floor(array.length / 2);
+  const left = array.slice(0, middleIndex);
+  const right = array.slice(middleIndex);
+  return merge(mergeSort(left), mergeSort(right));
+}
 
+function merge(left, right) {
+  let array = [];
+
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      array.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      array.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+
+  return array.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
 
 // No modificar nada debajo de esta línea
