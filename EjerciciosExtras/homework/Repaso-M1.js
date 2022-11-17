b49443b523476a56ed595d98d99ef01d5d6e148a
Pato Programador <spartan117947@hotmail.com>
2022-11-17 18:59:06 -0500

    Solucion del repaso, contiene ejercicios con Arrays,contProps, queues, closures y arbol binario

--- EjerciciosExtras/homework/Repaso-M1.js
+++ EjerciciosExtras/homework/Repaso-M1.js
@@ -14,9 +14,17 @@ const {
 // Pista: utilizar el método Array.isArray() para determinar si algun elemento de array es un array anidado
 // [Para más información del método: https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Array/isArray]
 
-var countArray = function(array) {
+var countArray = function (array) {
     // Tu código aca:
-    
+    var sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (Array.isArray(array[i])) {
+            sum += countArray(array[i]);
+        } else {
+            sum += array[i];
+        }
+    }
+    return sum;
 }
 
 
@@ -37,64 +45,90 @@ var countArray = function(array) {
 // dentro de a tenemos 3 propiedades mas, luego a3 tiene otras 3 y por ultimo c tiene una extra.
 // Propiedades: a, a1, a2, a3, f, a, c, o, b, c --> 10 en total
 
-var countProps = function(obj) {
-    // Tu código aca:
-
+var countProps = function (obj) {
+    let count = 0;
+    for (const prop in obj) {
+        count ++;
+        if(typeof obj[prop] === "object" && !Array.isArray(obj[prop])) {
+            count += countProps(obj[prop]);
+        }
+    }
+    return count;
 }
 
 
-// Implementar el método changeNotNumbers dentro del prototype de LinkedList que deberá cambiar
-// aquellos valores que no puedan castearse a numeros por 'Kiricocho' y devolver la cantidad de cambios que hizo
-// Aclaracion: si el valor del nodo puede castearse a número NO hay que reemplazarlo
-// Ejemplo 1:
-//    Suponiendo que la lista actual es: Head --> [1] --> ['2'] --> [false] --> ['Franco']
-//    lista.changeNotNumbers();
-//    Ahora la lista quedaría: Head --> [1] --> ['2'] --> [false] --> ['Kirikocho] y la función debería haber devuelto el valor 1
+    // Implementar el método changeNotNumbers dentro del prototype de LinkedList que deberá cambiar
+    // aquellos valores que no puedan castearse a numeros por 'Kiricocho' y devolver la cantidad de cambios que hizo
+    // Aclaracion: si el valor del nodo puede castearse a número NO hay que reemplazarlo
+    // Ejemplo 1:
+    //    Suponiendo que la lista actual es: Head --> [1] --> ['2'] --> [false] --> ['Franco']
+    //    lista.changeNotNumbers();
+    //    Ahora la lista quedaría: Head --> [1] --> ['2'] --> [false] --> ['Kirikocho] y la función debería haber devuelto el valor 1
 
-LinkedList.prototype.changeNotNumbers = function(){
-    // Tu código aca:
-
-}
-
-
-// Implementar la función mergeQueues que a partir de dos queues recibidas por parametro
-// debe devolver una nueva Queue que vaya mergeando los nodos de las anteriores.
-// Ejemplo:
-// - queueOne: [7,3,5]
-// - queueTwo: [2,4,6]
-// mergeQueues(queueOne, queueTwo) --> [7,2,3,4,5,6]
-// IMPORTANTE: NO son arreglos sino que son Queues.
-
-var mergeQueues = function(queueOne, queueTwo) {
-    // Tu código aca:
-
-}
+    LinkedList.prototype.changeNotNumbers = function () {
+        // Tu código aca:
+        let count = 0;
+        let current = this.head;
+        while(current) {
+            if(isNaN(current.value)) {
+                count++;
+                current.value = "Kiricocho";
+            }
+            current = current.next;
+        }
+        return count;
+    }
 
 
-// Implementar la funcion closureMult que permita generar nuevas funciones que representen
-// las tablas de multiplicación de distintos numeros
-// Ejemplo: 
-// - var multByFour = closureMult(4);
-// - multByFour(2) --> 8 (2 * 4)
-// - multByFour(5) --> 20
-// - var multBySix = closureMult(6);
-// - multBySix(4) --> 24
+    // Implementar la función mergeQueues que a partir de dos queues recibidas por parametro
+    // debe devolver una nueva Queue que vaya mergeando los nodos de las anteriores.
+    // Ejemplo:
+    // - queueOne: [7,3,5]
+    // - queueTwo: [2,4,6]
+    // mergeQueues(queueOne, queueTwo) --> [7,2,3,4,5,6]
+    // IMPORTANTE: NO son arreglos sino que son Queues.
 
-var closureMult = function(multiplier) {
-    // Tu código aca:
+    var mergeQueues = function (queueOne, queueTwo) {
+        // Tu código aca:
+        const newQueue = new Queue();
+        while(queueOne.size()||queueTwo.size()){
+            if(queueOne.size()) newQueue.enqueue(queueOne.dequeue());
+            if(queueTwo.size()) newQueue.enqueue(queueTwo.dequeue());
+        }
+        return newQueue;
+    }
 
-}
 
-// Implementar el método sum dentro del prototype de BinarySearchTree
-// que debe retornar la suma total de los valores dentro de cada nodo del arbol
-BinarySearchTree.prototype.sum = function() {
-    // Tu código aca:
+    // Implementar la funcion closureMult que permita generar nuevas funciones que representen
+    // las tablas de multiplicación de distintos numeros
+    // Ejemplo: 
+    // - var multByFour = closureMult(4);
+    // - multByFour(2) --> 8 (2 * 4)
+    // - multByFour(5) --> 20
+    // - var multBySix = closureMult(6);
+    // - multBySix(4) --> 24
 
-}
+    var closureMult = function (multiplier) {
+        // Tu código aca:
+        return function (num) {
+            return num * multiplier;
+        }
+    }
 
-module.exports = {
-    countArray,
-    countProps,
-    mergeQueues,
-    closureMult
-}
+    // Implementar el método sum dentro del prototype de BinarySearchTree
+    // que debe retornar la suma total de los valores dentro de cada nodo del arbol
+    BinarySearchTree.prototype.sum = function () {
+        // Tu código aca:
+        let count = 0;
+        count += this.value;
+        if(this.left) count += this.left.sum();
+        if(this.right) count += this.right.sum();
+        return count;
+    }
+
+    module.exports = {
+        countArray,
+        countProps,
+        mergeQueues,
+        closureMult
+    }
